604a6829fc618489de54c4aceac66bcf4be0a43e
Yaroslav Ustinov <y.ustinov2004@gmail.com>
2025-10-08 15:56:30 +0200

    feat: add drf-spectacular for API schema and Swagger docs

--- src/config/urls.py
+++ src/config/urls.py
@@ -17,8 +17,18 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),  # admin endpoints
     path("api/v1/", include("core.urls")),  # API endpoints
+    # API documentation endpoints
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # Optional UI:
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
